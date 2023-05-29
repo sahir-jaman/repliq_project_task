4d5c6340e1dc42ab7013c636391e2baeac0dcd15
sahir jaman <55667189+sahir-jaman@users.noreply.github.com>
2023-05-29 14:56:38 +0600

    Update urls.py

--- repliq_api/urls.py
+++ repliq_api/urls.py
@@ -5,5 +5,5 @@ from .views import home_page
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', home_page),
-    path('sahir_api/', include('sahir_api.urls')),
+    path('sahir_api/', include('sahir_api.urls')), # linking to the sahir_api.urls 
 ]
